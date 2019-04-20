0ccec6b3ee2079b5b6c26598af68b42dfe1f8d13
gaoliwen <>
2019-04-20 19:38:02 +0800

    delete code

--- saishizhichuang/main.cpp
+++ saishizhichuang/main.cpp
@@ -35,62 +35,11 @@ int main(int argc, const char * argv[]) {
     av_register_all();
     avformat_network_init();
     
-//    //上下文
-//    AVFormatContext *pFormatCtx = nullptr;//format context
-//    AVCodecContext *pCodecCtx = nullptr;//codec context
-//    AVCodec *pCodec = nullptr;//codec
-//    AVFrame    *pFrame = nullptr,
- //   AVFrame *pFrameYUV = nullptr;//frame 编码前的数据，如yuv数据
-//    AVPacket *packet = nullptr;//编码后的数据，如h264数据
-//    struct SwsContext *img_convert_ctx = nullptr;//图像转换上下文
-    
     //SDL相关的变量
     int screen_w = 1920, screen_h = 1080;//显示的宽和高
     SDL_Window *screen = nullptr;
     SDL_Rect rect;
     SDL_Event event;
-    
-//    //format 上下文初始化
-//    pFormatCtx = avformat_alloc_context();
-//    
-//    AVDictionary *format_opts = NULL;
-//    //av_dict_set(&format_opts, "stimeout", std::to_string( 2* 1000000).c_str(), 0); //设置链接超时时间（us）
-//    av_dict_set(&format_opts, "rtsp_transport",  "tcp", 0); //设置推流的方式，默认udp。
-//    
-//    int ret = avformat_open_input(&pFormatCtx, filepath, nullptr, &format_opts);
-//    std::cout<<"avformat_open_input result is "<<ret<<std::endl;
-//    
-//    ret = avformat_find_stream_info(pFormatCtx, nullptr);
-//    std::cout<<"avformat_find_stream_info result is "<<ret<<std::endl;
-//    
-//    //查找视频流索引
-//    int videoindex = -1;
-//    for(int i=0; i<pFormatCtx->nb_streams; i++){
-//        if(pFormatCtx->streams[i]->codec->codec_type==AVMEDIA_TYPE_VIDEO){
-//            videoindex = i;
-//            break;
-//        }
-//    }
-//    std::cout<<"videoindex is "<<videoindex<<std::endl;
-//    
-//    //codec 上下文初始化
-//    pCodecCtx=pFormatCtx->streams[videoindex]->codec;
-//    //codec 初始化
-//    //to do : 确认解码器是否需要释放
-//    pCodec=avcodec_find_decoder(pCodecCtx->codec_id);
-//    std::cout<<"pCodecCtx->codec_id is "<< pCodecCtx->codec_id<<" (如果是27，就表示是h264编码)"<<std::endl;
-//    
-//    //打开解码器
-//    ret = avcodec_open2(pCodecCtx, pCodec, nullptr);
-//    std::cout<<"avcodec_open2 result is "<<ret<<std::endl;
-//    
-//    //frame初始化
-//    pFrame = av_frame_alloc();
-    //frame初始化
-//    pFrameYUV = av_frame_alloc();
-//    
-//    uint8_t *out_buffer = new uint8_t[avpicture_get_size(AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height)];//分配AVFrame所需内存
-//    avpicture_fill((AVPicture *)pFrameYUV, out_buffer, AV_PIX_FMT_YUV420P, pCodecCtx->width, pCodecCtx->height);//填充AVFrame
     
     //SDL2初始化
     int ret = SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_TIMER);
@@ -102,42 +51,6 @@ int main(int argc, const char * argv[]) {
     SDL_Texture *sdlTexture = SDL_CreateTexture(sdlRenderer, SDL_PIXELFORMAT_IYUV,
                                                 SDL_TEXTUREACCESS_STREAMING,
                                                 screen_w, screen_h);
-    
-    //to do
-    //逻辑
-    
-//    //packet初始化
-//    packet = (AVPacket*)av_malloc(sizeof(AVPacket));
-//    
-//    //打印一下输入文件的信息
-//    av_dump_format(pFormatCtx, 0, filepath, 0);
-//    
-//    //初始化图像转换上下文
-//    img_convert_ctx = sws_getContext(pCodecCtx->width, pCodecCtx->height, pCodecCtx->pix_fmt,
-//                                     pCodecCtx->width, pCodecCtx->height, AV_PIX_FMT_YUV420P, SWS_BICUBIC,
-//                                     NULL, NULL, NULL);
-
-    //主逻辑
-//    int got_picture = -1;
-//    while (av_read_frame(pFormatCtx, packet)>=0) {
-//        if(packet->stream_index == videoindex){
-//            //解码：h264->yuv
-//            ret = avcodec_decode_video2(pCodecCtx, pFrame, &got_picture, packet);
-//            if(ret<0){
-//                std::cout<<"严重错误"<<std::endl;
-//                //return -1;
-//                break;
-//            }
-//            if(got_picture){//解码成功
-//                static int count = 0;
-//                std::cout<<count++<<" frame"<<std::endl;
-//                
-//                //像素格式转换
-//                sws_scale(img_convert_ctx,
-//                          (const uint8_t* const*)pFrame->data, pFrame->linesize,
-//                          0, pCodecCtx->height,
-//                          pFrameYUV->data, pFrameYUV->linesize);
-//
     
     RTSPSource rtspSource;
     rtspSource.Init();
@@ -184,17 +97,7 @@ int main(int argc, const char * argv[]) {
         }
 
     }
-//                //////////
-//                //@begin
 
-//                //@end
-//                //////////
-//            }
-//        }//end of "if(packet->stream_index == videoindex)"
-//        av_free_packet(packet);
-
-    
-    //while (true) {
         SDL_PollEvent(&event);
         switch( event.type ) {
             case SDL_QUIT:
@@ -204,29 +107,9 @@ int main(int argc, const char * argv[]) {
             default:
                 break;
         }
-
-    //}
-    
-    
-    //}
-    
-//    sws_freeContext(img_convert_ctx);
-//
-    
     
     rtspSource.Stop();
     
-    SDL_DestroyTexture(sdlTexture);
-//    delete[] out_buffer;
-//    av_free(pFrameYUV);
-//
-//    //avcodec_free_context(&pCodecCtx);//不需要
-//    avcodec_close(pCodecCtx);
-//    pCodecCtx = nullptr;
-//    
-//    avformat_close_input(&pFormatCtx);
-//    avformat_free_context(pFormatCtx);
-//    pFormatCtx = nullptr;
-    
+    SDL_DestroyTexture(sdlTexture);    
     return 0;
 }
